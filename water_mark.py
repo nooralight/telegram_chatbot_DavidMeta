a544c7d89ae504947603ee68ddc351bc87836bef
Noor Muhammad Light <77665556+nooralight@users.noreply.github.com>
2023-02-09 02:10:48 +0600

    Update water_mark.py

--- water_mark.py
+++ water_mark.py
@@ -9,21 +9,24 @@ class Water_Mark:
     def get_result(self):
         
         response = requests.get(self.url)
-        img = Image.open(BytesIO(response.content)) 
+        img = Image.open(BytesIO(response.content))
         #Creating draw object
-        draw = ImageDraw.Draw(img) 
+        draw = ImageDraw.Draw(img)
 
         #Creating text and font object
         text = "GenAiToken.com"
-        font = ImageFont.truetype('arial.ttf', 20)
-
+        font = ImageFont.truetype('dejavu/GothamBold.ttf', 15)
+        text_size = draw.textsize(text,font=font)
         #Positioning Text
-        
-        x=0
-        y=0
-
+        x=6
+        y=6
+        # Add a shadow border to the text
+        for offset in range(1, 2):
+            draw.text((x - offset, y), text, font=font, fill=(88, 88, 88))
+            draw.text((x + offset, y), text, font=font, fill=(88, 88, 88))
+            draw.text((x, y + offset), text, font=font, fill=(88, 88, 88))
+            draw.text((x, y - offset), text, font=font, fill=(88, 88, 88))
         #Applying text on image via draw object
-        draw.text((x, y), text, font=font) 
-
+        draw.text((x, y), text, font=font,fill=(255,255,255))
         #Saving the new image
         img.save("result.png")
